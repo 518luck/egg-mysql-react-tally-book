39d6e291a22f5107787c1d309d706af75f4fcf88
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-09-06 20:29:35 +0800

    refactor(config): 修改默认数据库名称从juejue-cost到bill

--- config/config.default.js
+++ config/config.default.js
@@ -54,7 +54,7 @@ module.exports = (appInfo) => {
       // 密码
       password: '123456', // 初始化密码，没设置的可以不写
       // 数据库名
-      database: 'juejue-cost', // 我们新建的数据库名称
+      database: 'bill',
     },
     // 是否加载到 app 上，默认开启
     app: true,
